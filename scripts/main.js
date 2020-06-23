a5116fc9204398fb32a56a6837b60ca20f9aa8d5
yvsn <yvsn@users.noreply.github.com>
2020-06-23 17:46:58 +0200

    Step 2
    Added Open Weather

--- scripts/main.js
+++ scripts/main.js
@@ -58,3 +58,59 @@ geojson.features.forEach(function(marker) {
     .setHTML('<h3>' + marker.properties.title + '</h3><p>' + marker.properties.description + '</p>'))
   .addTo(map);
 });
+
+
+
+function getAPIdata() {
+
+	var url = 'https://api.openweathermap.org/data/2.5/weather';
+	var apiKey ='3d3b923a70391c9c73f2c77695e5bec3';
+	var city = document.getElementById('inputCity').value;
+
+	// construct request
+	var request = url + '?' + 'appid=' + apiKey + '&' + 'q=' + city;
+
+	// get current weather
+	fetch(request)
+
+	// parse to JSON format
+	.then(function(response) {
+		if(!response.ok) throw Error(response.statusText);
+		return response.json();
+	})
+
+	// render weather per day
+	.then(function(response) {
+		// render weatherCondition
+		onAPISucces(response);
+	})
+
+	// catch error
+	.catch(function (error) {
+		onAPIError(error);
+	});
+}
+
+
+function onAPISucces(response) {
+	// get type of weather in string format
+	var description = response.weather[0].description;
+
+	// get temperature in Celcius
+	var degrees = Math.floor(response.main.temp - 273.15);
+
+	// render weather in DOM
+	var weatherBox = document.getElementById('weather');
+	weatherBox.innerHTML = response.name + '<br>' + degrees + '&#176;C <br>' + description;
+}
+
+
+function onAPIError(error) {
+	var weatherBox = document.getElementById('weather');
+	weatherBox.innerHTML = 'Try again! <br/> It looks like you enter an invalid city name :(';
+}
+
+// init data stream
+document.getElementById('getWeather').onclick = function(){
+	getAPIdata();
+};
